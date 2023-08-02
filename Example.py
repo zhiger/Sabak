ac0a973eeec02eb39fc41c6c23790382fbc96bc5
Zhiger Tungishbaev <zhiger2007@gmail.com>
2023-08-02 22:37:55 +0600

    Added new function, returns multiplication by 2

--- Example.py
+++ Example.py
@@ -2,4 +2,10 @@ import numpy as np
 
 a = np.random.randint(0, 100, size=10)
 b = [ value for value in a if value==1]
-print(len(b))
+c = len(b)
+# print(c*2)
+
+def func(param):
+    return param*2
+
+print(func(c))
