588bdbd1524781fd24d0b0475b7e2a271c47b15b
Zhiger Tungishbaev <zhiger2007@gmail.com>
2023-08-02 15:47:11 +0600

    First git commit

---
+++ Sabak-21-Modul-OS.py
@@ -0,0 +1,30 @@
+import os
+
+#os.remove(r"D:\Data science\test.txt")
+#os.mkdir(r'D:\folder\test4')
+#os.removedirs(r'D:\folder\test')
+#os.startfile(r'D:\folder\test1.txt')
+#os.rmdir(r'D:\folder\test2')
+#print(os.path.basename('D:\folder\test1.txt'))
+#print(os.path.dirname('D:/folder/test1.txt'))
+#print(os.path.getsize("D:/folder/test1.txt"))
+#os.rename(r'D:\folder\test1.txt', r'D:\folder\test11.txt')
+#os.rename(r'D:\\folder', r'D:\\catalog')
+#os.renames(r'D:\catalog\test3', r'D:\folder\test33')
+#os.startfile(r'D:\folder\test1.txt')
+#print(os.listdir(r'D:\folder'))
+# print(os.stat(r'D:\folder\test1.txt'))
+#print("Текущая деректория:", os.getcwd())
+# os.mkdir(r'D:\folder\test22\test21')
+# os.replace('D:\catalog\test2.tx')
+
+# Функция os.path.split делает разделение пути на «основную часть» и конец пути по
+# последнему / и возвращает кортеж из двух элементов.
+# os.path.split('D:/catalog/test4/test2.txt')
+# print(os.path.split('D:\catalog\test4\test2.txt')) #
+
+# Функция os.path.abspath возвращает абсолютный путь для указанного файла или каталога:
+# os.path.abspath('Example\Examples.ipynb')
+
+
+#%%
